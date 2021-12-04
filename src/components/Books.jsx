8a978f310e1eabd4e08a4fd5e1de6f74d28eb9ec
ale_figure8 <gomeznieto@gmail.com>
2021-12-04 20:06:09 -0300

    Adding props

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -13,6 +13,9 @@ function books() {
         <p className="font-bold mb-3 text-gray-700 uppercase">Date: {'\n'}
             <span className="font-normal normal-case">10 de Diciembre de 2021</span>
         </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Rate: {'\n'}
+            <span className="font-normal normal-case">3.5</span>
+        </p>
         <p className="font-bold mb-3 text-gray-700 uppercase">Sunopsis: {'\n'}
             <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quis earum placeat cum harum laboriosam tempora dolor dolore excepturi, ea quo, sed beatae exercitationem voluptates aliquid reiciendis necessitatibus hic dolorum corrupti? Laborum tempora sed, similique unde minima maiores inventore dolor!</span>
         </p>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,75 @@
+import { useState, useEffect } from 'react'
+
 function Form() {
+    const [book, setBook] = useState('')
+    const [author, setAuthor] = useState('')
+    const [genre, setGenre] = useState('')
+    const [date, setDate] = useState('')
+    const [rate, setRate] = useState('')
+    const [synopsis, setSynopsis] = useState('')
+
+    const [error, setError] = useState(false)
+
+    function handleValue (e) {
+        const inputvalue = e.target.value
+        const state = (e.target.id)
+        switch (state) {
+            case 'book':
+                setBook(inputvalue)
+                break
+            case 'author':
+                setAuthor(inputvalue)
+                break          
+            case 'genre':
+                setGenre(inputvalue)
+                break          
+            case 'date':
+                setDate(inputvalue)
+                break          
+            case 'rate':
+                setRate(inputvalue)
+                break         
+            case 'synopsis':
+                setSynopsis(inputvalue)
+                break              
+        }
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        
+      if([book, author, genre, date, rate, synopsis].includes('')){
+           return setError(true)
+        } 
+
+        setError(false)
+
+        cleanForm()
+    }
+
+    function cleanForm () {
+        setBook('')
+        setAuthor('')
+        setGenre('')
+        setDate('')
+        setRate('')
+        setSynopsis('')
+    }
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="text-3xl font-black text-center">What i have been reading...</h2>
             <p className="text-lg mt-5 text-center mb-10">Add a {'\n'} <span className="text-indigo-600 font-bold"> Book</span></p>
-            <form className="bg-white shadow-md rounded-lg py-10 px-5">
+            <form 
+                className="bg-white shadow-md rounded-lg py-10 px-5"
+                onSubmit={handleSubmit}
+            >
+                {error && <p 
+                            className="text-center text-red-700 font-bold pb-2">
+                            Debe completar todos los datos
+                          </p>
+                }
+
                 <div className="mb-5">
                     <label 
                         htmlFor="book" 
@@ -16,6 +82,8 @@ function Form() {
                         id="book" 
                         placeholder="Add book title"
                         className="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                        value={book}
+                        onChange={handleValue}
                     />
                 </div>
                 <div className="mb-5">
@@ -30,6 +98,8 @@ function Form() {
                         id="author" 
                         placeholder="Add author"
                         className="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                        value={author}
+                        onChange={handleValue}
                     />
                 </div>
                 <div className="mb-5">
@@ -44,6 +114,8 @@ function Form() {
                         id="genre" 
                         placeholder="Add genre"
                         className="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                        value={genre}
+                        onChange={handleValue}
                     />
                 </div>
                 <div className="mb-5">
@@ -57,6 +129,26 @@ function Form() {
                         name="date" 
                         id="date" 
                         className="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                        value={date}
+                        onChange={handleValue}
+                    />
+                </div>
+                <div className="mb-5">
+                    <label 
+                        htmlFor="rate" 
+                        className="block mb-2 text-gray-700 uppercase font-bold">
+                        Rate
+                    </label>
+                    <input 
+                        type="number" 
+                        min="1" 
+                        max="5"
+                        step="0.25"
+                        name="rate" 
+                        id="rate" 
+                        className="border-2 w-full p-2 placeholder-gray-400 rounded-md"
+                        value={rate}
+                        onChange={handleValue}
                     />
                 </div>
                 <div className="mb-5">
@@ -69,7 +161,12 @@ function Form() {
                         id="synopsis"
                         className="border-2 w-full p-2 placeholder-gray-400 rounded-md"
                         placeholder="A bref synopsis og the book"
-                    />
+                        name="synopsis"
+                        value={synopsis}
+                        onChange={handleValue}
+                    >
+                        {synopsis}
+                    </textarea>
                 </div>
                 <input 
                 type="submit" 
